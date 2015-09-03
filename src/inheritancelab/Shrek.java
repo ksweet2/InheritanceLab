b914aa70f6ead030e0f80c9834e92b57744b4d3a
L115student <L115student@L115-01-041977.bitnet.wctc.edu>
2015-09-03 13:03:05 -0500

    First Commit

---
+++ src/inheritancelab/Shrek.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancelab;
+
+/**
+ *
+ * @author L115student
+ */
+public class Shrek implements Ogre {
+    private String roar;
+
+    public String getRoar() {
+        return roar;
+    }
+
+    public void setRoar(String roar) {
+        this.roar = roar;
+    }
+    
+}
---
+++ src/inheritancelab/SynysterGatesCustom.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancelab;
+
+/**
+ *
+ * @author L115student
+ */
+public class SynysterGatesCustom implements ElectricGuitar{
+    private String play;
+
+    public String getPlay() {
+        return play;
+    }
+
+    public void setPlay(String play) {
+        this.play = play;
+    }
+    
+    
+}
